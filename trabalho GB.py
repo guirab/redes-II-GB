1580557d95f42f5e0a7a0515eb19d34feb82af1a
guirab <guilhermesr2000@gmail.com>
2023-06-14 17:08:08 -0300

    fix: add sensing timer

--- trabalho GB.py
+++ trabalho GB.py
@@ -4,6 +4,8 @@ import time
 class Transmissor:
     def __init__(self, id):
         self.id = id
+        self.tempo_inicio_sensing = random.uniform(0.0, 2.0)
+        self.duracao_sensing = random.uniform(0.5, 2.0)
 
     def verificar_meio(self):
         return random.choice([True, False])  # Simula o meio de transmissão ocupado ou livre
@@ -14,25 +16,29 @@ class Transmissor:
     def backoff_algorithm(self, tentativa):
         espera = random.randint(0, (2 ** tentativa) - 1)
         print(f"Transmissor {self.id} esperando por {espera} segundos")
-
         time.sleep(espera)  # Espera um tempo exponencial truncado
 
     def transmitir(self, dados):
-        while True:
-            if self.verificar_meio():
-                self.colisao_detectada()
-                tentativa = 0
+        # Espera o tempo de início do sensing
+        time.sleep(self.tempo_inicio_sensing)
+        print(f"Transmissor {self.id} começou o sensing")
 
-                while tentativa < 10:  # Limite máximo de tentativas
-                    self.backoff_algorithm(tentativa)
-                    if not self.verificar_meio():
-                        break
-                    tentativa += 1
-                else:
-                    print(f"Transmissor {self.id} não conseguiu transmitir após várias tentativas")
+        # Realiza o sensing por um tempo específico
+        time.sleep(self.duracao_sensing)
+
+        if self.verificar_meio():
+            self.colisao_detectada()
+            tentativa = 0
+
+            while tentativa < 10:  # Limite máximo de tentativas
+                self.backoff_algorithm(tentativa)
+                if not self.verificar_meio():
+                    break
+                tentativa += 1
             else:
-                print(f"Transmissor {self.id}, dados {dados} com sucesso")
-                break
+                print(f"Transmissor {self.id} não conseguiu transmitir após várias tentativas")
+        else:
+            print(f"Transmissor {self.id}, dados {dados} transmitidos com sucesso")
 
 # Criando os transmissores
 transmissor1 = Transmissor(1)
