6a171b978820a7ac1604ab2a2a8ab2bb57eebaf1
guirab <guilhermesr2000@gmail.com>
2023-06-23 10:42:09 -0300

    refactor

--- trabalho GB.py
+++ trabalho GB.py
@@ -2,75 +2,64 @@ import random
 import time
 import threading
 
-class Transmissor:
-    def __init__(self, id):
-        self.id = id
-        self.tempo_inicio_sensing = random.uniform(0.0, 2.0)
-        self.duracao_sensing = random.uniform(0.5, 2.0)
-        self.thread = None
-        self.transmitindo = False
+class Canal:
+    def __init__(self):
+        self.ocupado = False
+        self.lock = threading.Lock()
 
     def verificar_meio(self):
-        return random.choice([True, False])  # Simula o meio de transmissão ocupado ou livre
+        with self.lock:
+            return self.ocupado
 
-    def colisao_detectada(self):
-        print(f"Colisão detectada no transmissor {self.id}")
+    def ocupar(self):
+        with self.lock:
+            self.ocupado = True
 
-    def backoff_algorithm(self, tentativa):
-        espera = random.randint(0, (2 ** tentativa) - 1)
-        print(f"Transmissor {self.id} esperando por {espera} segundos")
-        time.sleep(espera)  # Espera um tempo exponencial truncado
+    def desocupar(self):
+        with self.lock:
+            self.ocupado = False
 
-    def transmitir(self, dados):
-        self.thread = threading.Thread(target=self.enviar_dados, args=(dados,))
-        self.thread.start()
+class Transmissor(threading.Thread):
+    def __init__(self, id, canal, tamanho):
+        super().__init__()
+        self.id = id
+        self.colisoes = 0
+        self.canal = canal
+        self.tamanho = tamanho
 
-    def enviar_dados(self, dados):
-        self.transmitindo = True
+    def backoff(self):
+        espera = random.randint(0, (2 ** self.colisoes) - 1)
+        print(f'Transmissor {self.id}: meio ocupado. Realizando backoff. Esperando {espera} segundos.')
+        time.sleep(espera)
 
-        # Espera o tempo de início do sensing
-        time.sleep(self.tempo_inicio_sensing)
-        print(f"Transmissor {self.id} começou o sensing")
-
-        # Realiza o sensing por um tempo específico
-        time.sleep(self.duracao_sensing)
-
+    def transmitir(self):
         while True:
-            if self.verificar_meio():
-                self.colisao_detectada()
-                tentativa = 0
-
-                while tentativa < 10:  # Limite máximo de tentativas
-                    self.backoff_algorithm(tentativa)
-                    if not self.verificar_meio():
-                        break
-                    tentativa += 1
-                else:
-                    print(f"Transmissor {self.id} não conseguiu transmitir após várias tentativas")
+            if self.canal.verificar_meio():
+                self.colisoes += 1
+                self.backoff()
             else:
-                print(f"Transmissor {self.id}, dados {dados} transmitidos com sucesso")
+                self.canal.ocupar()
+                print(f'Transmissor {self.id}: meio livre. Enviando dados. Tempo de transmissão: {self.tamanho} segundos.')
+                time.sleep(self.tamanho)
+                print(f'Transmissor {self.id}: dados enviados com sucesso.')
+                self.canal.desocupar()
                 break
 
-        self.transmitindo = False
+    def run(self):
+        self.transmitir()
 
-# Criando os transmissores
-transmissor1 = Transmissor(1)
-transmissor2 = Transmissor(2)
-transmissor3 = Transmissor(3)
-transmissor4 = Transmissor(4)
+canal = Canal()
+transmissor1 = Transmissor(1, canal, 10)
+transmissor2 = Transmissor(2, canal, 15)
+transmissor3 = Transmissor(3, canal, 10)
+transmissor4 = Transmissor(4, canal, 10)
 
-# Definindo os dados dos quadros
-dados_transmissor1 = "transmissor 1"
-dados_transmissor2 = "transmissor 2"
-dados_transmissor3 = "transmissor 3"
-dados_transmissor4 = "transmissor 4"
+transmissor1.start()
+transmissor2.start()
+transmissor3.start()
+transmissor4.start()
 
-# Iniciando a simulação
-transmissor1.transmitir(dados_transmissor1)
-transmissor2.transmitir(dados_transmissor2)
-transmissor3.transmitir(dados_transmissor3)
-transmissor4.transmitir(dados_transmissor4)
-
-# Aguardando a finalização das transmissões
-while transmissor1.transmitindo or transmissor2.transmitindo or transmissor3.transmitindo or transmissor4.transmitindo:
-    time.sleep(0.1)
+transmissor1.join()
+transmissor2.join()
+transmissor3.join()
+transmissor4.join()
